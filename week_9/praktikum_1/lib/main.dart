d279b5d552ee386727fd1d62ace7db479a06f31f
soulqan <soultan.agnar@gmail.com>
2025-10-26 18:07:11 +0700

    push

---
+++ week_9/praktikum_1/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:week_9/widget/take_picture_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final cameras = await availableCameras();
+  final firstCamera = cameras.first;
+
+  runApp(
+    MaterialApp(
+      theme: ThemeData.dark(),
+      home: TakePictureScreen(camera: firstCamera),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
