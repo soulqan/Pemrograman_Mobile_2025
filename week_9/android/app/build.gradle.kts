b9a5bf9577925468292b499edf783d9543071535
soulqan <soultan.agnar@gmail.com>
2025-10-26 11:01:03 +0700

    ..

--- week_9/android/app/build.gradle.kts
+++ week_9/android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
     applicationId = "com.example.week_9"
-    minSdk = 21
+    minSdk = flutter.minSdkVersion
     targetSdk = flutter.targetSdkVersion
     versionCode = flutter.versionCode
     versionName = flutter.versionName
