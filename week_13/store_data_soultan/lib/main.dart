85fce570137c8c6ce1df69e854350afa9ecbc6cc
soulqan <soultan.agnar@gmail.com>
2025-11-21 17:37:59 +0700

    W13: Jawaban Soal 1

---
+++ week_13/store_data_soultan/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter JSON Demo Soultan',
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('JSON')),
+      body: Container(),
+    );
+  }
+}
